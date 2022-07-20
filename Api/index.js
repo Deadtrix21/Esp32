8564cfde8fb9c329e4e2a08f8b9fcc3e1b9c4a58
DeadTrix21 <whitequeenluna@gmail.com>
2022-07-21 01:32:46 +0200

    New PAck Update with Router in action

--- Api/index.js
+++ Api/index.js
@@ -1,16 +1,21 @@
 import ExpressMainFunction from 'express';
 const { express } = ExpressMainFunction;
 
-const application = ExpressMainFunction()
+const ExpressApplication = ExpressMainFunction()
+const ExpressRouter = ExpressMainFunction.Router()
 const port = 3000
 
 
+// Example Using Router
+import birds from "../Api/birds.js"; 
+ExpressApplication.use('/birds', birds)
 
-application.get("/", (req, res)=>{res.send('Hello World!')})
-application.get("/index", (req, res)=>{res.send('IndexPage')})
 
+ExpressApplication.get('/', (req, res) => {
+    res.send('hello world')
+  })
 
-application.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+ExpressApplication.listen(port, () => {
+    console.log(`Example app listening at: \nhttp://localhost:${port}`)
     }
 )
