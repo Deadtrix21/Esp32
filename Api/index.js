6685213537a247655da4d321857eec0d4c998aaf
DeadTrix21 <whitequeenluna@gmail.com>
2022-07-20 22:47:36 +0200

    Fix Push

--- Api/index.js
+++ Api/index.js
@@ -1,15 +1,14 @@
-import Express from 'express';
-const { express } = Express;
+import ExpressMainFunction from 'express';
+const { express } = ExpressMainFunction;
 
-const application = express()
+const application = ExpressMainFunction()
 const port = 3000
 
 
 
-
-
-
 application.get("/", (req, res)=>{res.send('Hello World!')})
+application.get("/index", (req, res)=>{res.send('IndexPage')})
+
 
 application.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
