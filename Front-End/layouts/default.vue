f1e9b8400d74a8752befc6d9e7df31a93cd068db
DeadTrix21 <whitequeenluna@gmail.com>
2022-07-21 13:38:15 +0200

    First Core Update

--- Front-End/layouts/default.vue
+++ Front-End/layouts/default.vue
@@ -1,92 +1,76 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
+    <v-app>
+        <v-navigation-drawer v-model="drawer" :mini-variant.sync="mini" permanent>
+            
+            <!--
+                ? This is the Title and Close Button
+                ! Start
+            -->
+            <v-list>
+                <section class="d-flex flex-row-reverse">
+                    <div class="mx-4">
+                        <v-btn icon @click.stop="mini = !mini" v-show="!mini"><v-icon>mdi-chevron-left</v-icon></v-btn>
+                    </div>
+                    <v-list-item v-show="!mini">
+                        <v-list-item-content>
+                            <v-list-item-title class="text-h6">
+                                Application
+                            </v-list-item-title>
+                            <v-list-item-subtitle>
+                                subtext
+                            </v-list-item-subtitle>
+                        </v-list-item-content>
+                    </v-list-item>
+                </section>
+                <v-divider v-show="!mini"></v-divider>
+            </v-list>
+            <!--
+                ? This is the Title and Close Button
+                ! End
+            -->
+               
+    <v-list dense nav>
         <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+          v-for="item in items"
+          :key="item.title"
+          link
         >
-          <v-list-item-action>
+          <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+          </v-list-item-icon>
+
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
-    </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
-  </v-app>
+        </v-navigation-drawer>
+    </v-app>
 </template>
 
+
 <script>
+
 export default {
-  name: 'DefaultLayout',
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
-  },
+    data () {
+      return {
+        img:null,
+        mini: true,
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+      }
+    },
 }
 </script>
+
+
+
+<style>
+::-webkit-scrollbar {
+    display: none;
+}
+      
+</style>
--- Front-End/nuxt.config.js
+++ Front-End/nuxt.config.js
@@ -59,7 +59,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -70,6 +70,15 @@ export default {
           error: colors.deepOrange.accent4,
           success: colors.green.accent3,
         },
+        light:{
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        }
       },
     },
   },
