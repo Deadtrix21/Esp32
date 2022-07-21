d9f9e5328e6fe13548cfefbd1325dbf2186fa717
DeadTrix21 <whitequeenluna@gmail.com>
2022-07-21 21:30:54 +0200

    My World

--- Front-End/pages/index.vue
+++ Front-End/pages/index.vue
@@ -5,10 +5,7 @@
 </template>
 
 <script>
-import NuxtLogo from '~/components/NuxtLogo.vue';
-import VuetifyLogo from '~/components/VuetifyLogo.vue';
 export default {
-  components: { NuxtLogo, VuetifyLogo },
   name: 'IndexPage',
 }
 </script>
