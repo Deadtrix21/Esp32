5164d7d46ba98eaf299f2b64fa41b4902f73ffb5
DeadTrix21 <whitequeenluna@gmail.com>
2022-07-25 02:17:28 +0200

    Login Update TS

---
+++ Front-End/components/Login.vue
@@ -0,0 +1,68 @@
+import { PropOptions } from 'vue';
+<template>
+  <div app>
+    <div>
+        Name: {{ User.FirstName }}
+    </div>
+    <section>
+      <!--
+                    // Note The core Data
+                -->
+      <v-text-field
+        v-model="User.FirstName"
+        label="Name"
+        placeholder="First Name"
+        :rules="rules.name"
+        filled
+        rounded
+        dense
+      >
+      </v-text-field>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+interface User{
+    FirstName   : string,
+    LastName    : string,
+    Email       : string,
+}
+
+
+
+class Person implements User{
+    FirstName   : string
+    LastName    : string
+    Email       : string
+
+    constructor(first:string = "", last:string = "", email:string = ""){
+        this.FirstName = first
+        this.LastName = last
+        this.Email = email
+    }
+}
+
+
+
+
+function VerifyName (val : string) : string | void {
+    if (val == null || val == ''){return "This Field Is REQUIRED"}
+    else if (val.length < 3){return "This Needs To Be More Than 3 Char"}
+    return;
+}
+
+export default {
+    data : function (){
+        return {
+            User : new Person(),
+            rules:{
+                name :[():string => {return VerifyName(this.User.FirstName);}]
+            }
+        }
+    },
+}
+</script>
+
+<style scoped></style>
+
--- Front-End/pages/index.vue
+++ Front-End/pages/index.vue
@@ -1,11 +1,15 @@
 <template>
 <div app>
   <h1>hello</h1>
+  <Login/>
 </div>
+
 </template>
 
 <script>
+import Login from '~/components/Login.vue';
 export default {
-  name: 'IndexPage',
+    name: "IndexPage",
+    components: { Login }
 }
 </script>
