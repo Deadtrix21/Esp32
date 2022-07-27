b7f4146c9f00c38fd24a7fb6843d06d21e50c482
DeadTrix <88140362+Deadtrix21@users.noreply.github.com>
2022-07-27 04:27:40 +0200

    Hello Wolrd

--- Front-End/ts/Login.ts
+++ Front-End/ts/Login.ts
@@ -10,7 +10,10 @@ interface Password{
     fPassword : string
 }
 
-
+interface iFormRules{
+    VerifyName  (val :string): string| void,
+    VerifyEmail (val :string): string| void
+}
 
 
 
@@ -18,6 +21,7 @@ interface Password{
 
 
 class Person implements User{
+
     FirstName   : string
     LastName    : string
     Email       : string
@@ -29,8 +33,9 @@ class Person implements User{
         this.Email = email
         this.Password = pws
     }
-}
 
+
+}
 
 class PasswordConfirm implements Password{
     fPassword :string = ""
@@ -41,34 +46,49 @@ class PasswordConfirm implements Password{
     public set_f (fpass:string) {
         this.fPassword = fpass
     }
+
+
     public set_rpass (rpass:string){
         this.rPassword = rpass
     }
+
+
     public match (): boolean{
         if (this.fPassword != this.rPassword){return false}
         else {return true}
     }
 }
 
-
-interface iFormRules{
-    VerifyName  (val :string): string| void,
-    VerifyEmail (val :string): string| void
-}
-
 class FormRules implements iFormRules{
     constructor(){}
+
+
     public VerifyName (val : string) : string | void {
         if (val == null || val == ''){return "This field is required."}
         else if (val.length < 3){return "This needs to be more than 3 char."}
         return;
     }
     
+
+
     public VerifyEmail (val :string): string | void{
         if (val == null || val == ''){return 'E-mail is required.'}
         else if (/.+@.+/.test(val)){return 'E-mail must be valid.'}
         return;
     }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
 //Exports
 export default {Person, PasswordConfirm, FormRules};
